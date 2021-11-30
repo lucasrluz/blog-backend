28f78e30b321da88b9d682fe70bebdf531201bb2
lucasrluz <lucasr.luzbr@gmail.com>
2021-11-30 14:27:48 -0300

    rota para buscar usuário

--- src/api/user/userRoute.ts
+++ src/api/user/userRoute.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
+import { findUserController } from './controllers/findUserController';
 import { saveUserController } from './controllers/saveUserController';
 
 export const userRoute = Router();
 
 userRoute.post('/user', saveUserController);
+userRoute.get('/user/:username', findUserController);
