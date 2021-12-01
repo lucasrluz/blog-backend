dd7e79e8fb802e44120575122715edc89003a460
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-01 20:06:57 -0300

    habilitando rotas para autenticar usuário

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { userRoute } from './api/user/userRoute';
+import { userAuthRoute } from './api/userAuth/userAuthRoute';
 
 export const app = express();
 
 app.use(express.json());
+app.use(userAuthRoute);
 app.use(userRoute);
