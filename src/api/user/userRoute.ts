4af2766cd0f35154b3fe19405429185fe09429fa
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-02 13:35:48 -0300

    rota para editar usuário

--- src/api/user/userRoute.ts
+++ src/api/user/userRoute.ts
@@ -1,4 +1,6 @@
 import { Router } from 'express';
+import { ensureAuthenticatedUser } from '../userAuth/middleware/ensureAuthenticatedUser';
+import { editUserController } from './controllers/editUserController';
 import { findUserController } from './controllers/findUserController';
 import { saveUserController } from './controllers/saveUserController';
 
@@ -6,3 +8,4 @@ export const userRoute = Router();
 
 userRoute.post('/user', saveUserController);
 userRoute.get('/user/:username', findUserController);
+userRoute.put('/user/:user_id', ensureAuthenticatedUser, editUserController);
