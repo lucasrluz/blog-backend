cda875a7965f62fbaf5435c30bb0ce3c2cc8dc29
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-02 13:32:46 -0300

    função na camada de service para editar usuário

---
+++ src/api/user/service/editUserService.ts
@@ -0,0 +1,35 @@
+import { hash } from 'bcrypt';
+import { createResponse } from '../../../response/createResponse';
+import { editUserRepository } from '../repositories/editUserRepository';
+import { findUserByUsernameAndIdRepository } from '../repositories/findUserByUsernameAndIdRepository';
+
+export async function editUserService(
+  userId: string,
+  data: { username: string; password: string },
+) {
+  const { username, password } = data;
+
+  const existingUser = await findUserByUsernameAndIdRepository(
+    username,
+    userId,
+  );
+
+  if (existingUser)
+    return createResponse(400, { message: 'This username is already in use' });
+
+  const passwordHash = await hash(password, 8);
+
+  const editUserResponse = await editUserRepository(userId, {
+    username,
+    password: passwordHash,
+  });
+
+  return createResponse(200, {
+    message: 'Successfully edited user',
+    object: {
+      id: editUserResponse.id,
+      username: editUserResponse.username,
+      email: editUserResponse.email,
+    },
+  });
+}
