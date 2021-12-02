b0f201ab0cf4267a772dff3ec187b9e574bea5df
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-02 13:34:50 -0300

    função na camada de controller para editar usuário

---
+++ src/api/user/controllers/editUserController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from 'express';
+import { editUserService } from '../service/editUserService';
+
+export async function editUserController(req: Request, res: Response) {
+  const userId = req.params.user_id;
+
+  const { username, password } = req.body;
+
+  const response = await editUserService(userId, { username, password });
+
+  return res.status(response.status).json(response.data);
+}
