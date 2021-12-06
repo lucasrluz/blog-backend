b0f27bf5f298abc4ae4e2d8c0bd033bf1a81edad
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-06 09:33:06 -0300

    testes na rota do refresh token

--- test/userAuth/userAuth.spec.ts
+++ test/userAuth/userAuth.spec.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import { app } from '../../src/app';
 import { prisma } from '../../src/prisma/prisma';
 import { users } from '../user/data/users';
+import { sleep } from './methods/sleep';
 import { validateToken } from './validateToken/validateToken';
 
 export function executeUserAuthTests() {
@@ -100,6 +101,84 @@ export function executeUserAuthTests() {
         expect(editUserResponse.body.message).toEqual('Token invalid');
 
         await prisma.user.deleteMany();
+      });
+    });
+
+    describe('/refresh-token (POST)', () => {
+      it('Should return refresh token', async () => {
+        await request(app).post('/user').send(users[0]);
+
+        const username = users[0].username;
+        const password = users[0].password;
+
+        const authenticateUserResponse = await request(app)
+          .post('/login')
+          .send({ username, password });
+
+        const oldToken = authenticateUserResponse.body.object.token;
+
+        const oldRefreshToken =
+          authenticateUserResponse.body.object.refreshToken.id;
+
+        const refreshTokenResponse = await request(app)
+          .post('/refresh-token')
+          .send(oldRefreshToken);
+
+        const newRefreshToken =
+          refreshTokenResponse.body.object.refreshToken.id;
+
+        const newToken = refreshTokenResponse.body.object.token;
+
+        expect(refreshTokenResponse.status).toEqual(200);
+        expect(newToken).toEqual(oldToken);
+        expect(oldRefreshToken).toEqual(newRefreshToken);
+
+        await prisma.refreshToken.deleteMany();
+        await prisma.user.deleteMany();
+      });
+
+      it('Should return new refresh token', async () => {
+        await request(app).post('/user').send(users[0]);
+
+        const username = users[0].username;
+        const password = users[0].password;
+
+        const authenticateUserResponse = await request(app)
+          .post('/login')
+          .send({ username, password });
+
+        const oldToken = authenticateUserResponse.body.object.token;
+
+        const oldRefreshToken =
+          authenticateUserResponse.body.object.refreshToken.id;
+
+        await sleep(20000);
+
+        const refreshTokenResponse = await request(app)
+          .post('/refresh-token')
+          .send(oldRefreshToken);
+
+        const newRefreshToken =
+          refreshTokenResponse.body.object.refreshToken.id;
+
+        const newToken = refreshTokenResponse.body.object.token;
+
+        expect(refreshTokenResponse.status).toEqual(200);
+        expect(newToken).not.toEqual(oldToken);
+        expect(oldRefreshToken).not.toEqual(newRefreshToken);
+
+        await prisma.refreshToken.deleteMany();
+      });
+
+      it('Should not return refresh token', async () => {
+        const refreshTokenResponse = await request(app)
+          .post('/refresh-token')
+          .send('refresh-token-invalid');
+
+        expect(refreshTokenResponse.status).toEqual(400);
+        expect(refreshTokenResponse.body.message).toEqual(
+          'Refresh token invalid',
+        );
       });
     });
   });
