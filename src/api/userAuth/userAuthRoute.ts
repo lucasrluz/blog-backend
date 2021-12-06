c9691fd56a0b67f21b30c50713665b62fcbf5d1c
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-06 09:28:56 -0300

    rota para criar refresh token

--- src/api/userAuth/userAuthRoute.ts
+++ src/api/userAuth/userAuthRoute.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { authenticateUserController } from './controller/authenticateUserController';
+import { generateRefreshTokenController } from './controller/generateRefreshTokenController';
 
 export const userAuthRoute = Router();
 
 userAuthRoute.post('/login', authenticateUserController);
+userAuthRoute.post('/refresh-token', generateRefreshTokenController);
