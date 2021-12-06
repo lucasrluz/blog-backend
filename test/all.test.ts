2f57620912a0a97e7987ce738b56f412f85b9831
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-06 09:37:13 -0300

    função de tempo limite para testes

--- test/all.test.ts
+++ test/all.test.ts
@@ -1,6 +1,8 @@
 import { executeUserTests } from './user/user.spec';
 import { executeUserAuthTests } from './userAuth/userAuth.spec';
 
+jest.setTimeout(50000);
+
 describe('All testes', () => {
   executeUserTests();
   executeUserAuthTests();
