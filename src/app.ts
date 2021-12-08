1b7b01a0ad23cf25f89355fa1e1a68921adfd21b
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-08 20:53:03 -0300

    habilitando rotas para post

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { postRoute } from './api/post/postRoute';
 import { userRoute } from './api/user/userRoute';
 import { userAuthRoute } from './api/userAuth/userAuthRoute';
 
@@ -7,3 +8,4 @@ export const app = express();
 app.use(express.json());
 app.use(userAuthRoute);
 app.use(userRoute);
+app.use(postRoute);
