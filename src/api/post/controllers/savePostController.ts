35cc730a62419c884ee1b40351f665a297990c1f
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-08 20:51:58 -0300

    função na camada de controller para criar novo post

---
+++ src/api/post/controllers/savePostController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { savePostService } from '../services/savePostService';
+
+export async function savePostController(req: Request, res: Response) {
+  const userId = req.params.user_id;
+
+  const { title, content } = req.body;
+
+  const post = {
+    title,
+    content,
+    userId,
+  };
+
+  const response = await savePostService(post);
+
+  return res.status(response.status).json(response.data);
+}
