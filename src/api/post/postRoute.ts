16f3e17f68cce2e4f05f39081b675961218a5866
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-10 13:28:42 -0300

    rota para buscar post

--- src/api/post/postRoute.ts
+++ src/api/post/postRoute.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { ensureAuthenticatedUser } from '../userAuth/middleware/ensureAuthenticatedUser';
+import { findPostController } from './controllers/findPostController';
 import { savePostController } from './controllers/savePostController';
 
 export const postRoute = Router();
 
 postRoute.post('/post/:user_id', ensureAuthenticatedUser, savePostController);
+postRoute.get('/post/:username/:post_title/:post_id', findPostController);
