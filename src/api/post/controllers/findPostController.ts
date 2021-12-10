a1ecb08aec2084f9a6a415ea40afbacef7d7f579
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-10 13:28:21 -0300

    função na camada de controller para busca de post

---
+++ src/api/post/controllers/findPostController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import { findPostService } from '../services/findPostService';
+
+export async function findPostController(req: Request, res: Response) {
+  const { post_id: postId, post_title: title, username } = req.params;
+
+  const response = await findPostService(postId, title, username);
+
+  res.status(response.status).json(response.data);
+}
