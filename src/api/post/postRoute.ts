9cceeef92faf00813a6f8c920e57226f299baec4
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-11 20:30:28 -0300

    rota para atualizar dados de post

--- src/api/post/postRoute.ts
+++ src/api/post/postRoute.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { ensureAuthenticatedUser } from '../userAuth/middleware/ensureAuthenticatedUser';
+import { editPostController } from './controllers/editPostController';
 import { findPostByUsernameController } from './controllers/findPostByUsernameController';
 import { findPostController } from './controllers/findPostController';
 import { savePostController } from './controllers/savePostController';
@@ -9,3 +10,8 @@ export const postRoute = Router();
 postRoute.post('/post/:user_id', ensureAuthenticatedUser, savePostController);
 postRoute.get('/post/:username/:post_title/:post_id', findPostController);
 postRoute.get('/post/:username', findPostByUsernameController);
+postRoute.put(
+  '/post/:user_id/:post_id',
+  ensureAuthenticatedUser,
+  editPostController,
+);
