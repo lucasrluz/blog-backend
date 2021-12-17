07244611ff40573e296e450dcf515fd4eeeb77aa
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-17 10:06:36 -0300

    função na camada de controller para cadastrar novo comment

---
+++ src/api/comment/controllers/saveCommentController.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { saveCommentService } from '../service/saveCommentService';
+
+export async function saveCommentController(req: Request, res: Response) {
+  const { user_id: userId } = req.params;
+  const { content, postId } = req.body;
+
+  const response = await saveCommentService({ content, userId, postId });
+
+  return res.status(response.status).json(response.data);
+}
