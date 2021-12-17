be515887ec6d27044b9f29a508a087299833eb91
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-17 10:08:59 -0300

    habilitando rotas para comment

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { commentRoute } from './api/comment/commentRoute';
 import { postRoute } from './api/post/postRoute';
 import { userRoute } from './api/user/userRoute';
 import { userAuthRoute } from './api/userAuth/userAuthRoute';
@@ -9,3 +10,4 @@ app.use(express.json());
 app.use(userAuthRoute);
 app.use(userRoute);
 app.use(postRoute);
+app.use(commentRoute);
