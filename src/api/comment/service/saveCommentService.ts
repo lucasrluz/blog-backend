ff43612b7329f1f00593f3d1e438d3338248c1d5
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-17 10:06:20 -0300

    função na camada de service para cadastrar novo comment

---
+++ src/api/comment/service/saveCommentService.ts
@@ -0,0 +1,23 @@
+import { createResponse } from '../../../response/createResponse';
+import { findPostByPostIdRepository } from '../../post/repositories/findPostByPostIdRepository';
+import { findUserByUserIdRepository } from '../../user/repositories/findUserByUserIdRepository';
+import { validateComment } from '../classValidator/validateComment';
+import { IComment } from '../interface/IComment';
+import { saveCommentRepository } from '../repositories/saveCommentRepository';
+
+export async function saveCommentService(comment: IComment) {
+  const commentValidation = await validateComment(comment);
+
+  if (commentValidation)
+    return createResponse(400, { message: commentValidation });
+
+  const { postId } = comment;
+
+  const existingPost = await findPostByPostIdRepository(postId);
+
+  if (!existingPost) return createResponse(404, { message: 'Post not found' });
+
+  const saveCommentResponse = await saveCommentRepository(comment);
+
+  return createResponse(201, { object: saveCommentResponse });
+}
