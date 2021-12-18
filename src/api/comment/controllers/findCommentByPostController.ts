99e0f6850f6d75ba0193748accef816a419eea0a
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-18 16:13:46 -0300

    função na camada de controller para buscar comment pelo post

---
+++ src/api/comment/controllers/findCommentByPostController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import { findCommentByPostService } from '../service/findCommentByPostService';
+
+export async function findCommentByPostController(req: Request, res: Response) {
+  const { post_id: postId } = req.params;
+
+  const response = await findCommentByPostService(postId);
+
+  return res.status(response.status).json(response.data);
+}
