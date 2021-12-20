0a1a9261f18f3ae525aba844966df5fa44529144
lucasrluz <lucasr.luzbr@gmail.com>
2021-12-20 19:41:44 -0300

    função na camada de controller para deletar comment

---
+++ src/api/comment/controllers/deleteCommentController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from 'express';
+import { deleteCommentService } from '../service/deleteCommentService';
+
+export async function deleteCommentController(req: Request, res: Response) {
+  const { user_id: userId, comment_id: commentId } = req.params;
+
+  const response = await deleteCommentService(commentId, userId);
+
+  return res.status(response.status).json(response.data);
+}
