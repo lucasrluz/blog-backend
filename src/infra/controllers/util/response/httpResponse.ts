0c9e17f6d24d0d02def604f078a4afb58ea26bb4
lucasrluz <lucasr.luzbr@gmail.com>
2022-03-03 18:25:30 -0300

    função com formato de resposta http

---
+++ src/infra/controllers/util/response/httpResponse.ts
@@ -0,0 +1,27 @@
+export function ok(value: any) {
+  return {
+    value,
+    statusCode: 200,
+  };
+}
+
+export function created(value: any) {
+  return {
+    value,
+    statusCode: 201,
+  };
+}
+
+export function badRequest(value: any) {
+  return {
+    value,
+    statusCode: 400,
+  };
+}
+
+export function notFound(value: any) {
+  return {
+    value,
+    statusCode: 404,
+  };
+}
