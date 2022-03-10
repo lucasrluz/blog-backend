7d6ebb527b3f970f6b5bfebafe201990e0edf95c
lucasrluz <lucasr.luzbr@gmail.com>
2022-03-10 09:23:10 -0300

    correção da validação dos dados do domínio comment

--- src/domain/comment/validate/validateComment.ts
+++ src/domain/comment/validate/validateComment.ts
@@ -2,11 +2,11 @@ import { error, success } from '../../../shared/response';
 import { IComment } from '../interface/IComment';
 
 export function validateComment(comment: IComment) {
-  if (comment.content.length === 0) return error('Content should not be empty');
+  if (!comment.content) return error('Content should not be empty');
 
-  if (comment.userId.length === 0) return error('UserId should not be empty');
+  if (!comment.userId) return error('UserId should not be empty');
 
-  if (comment.postId.length === 0) return error('PostId should not be empty');
+  if (!comment.postId) return error('PostId should not be empty');
 
   return success(comment);
 }
